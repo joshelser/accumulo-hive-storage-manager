9bba70ed39ec3a2967610616042b6fb8cfb74603
Josh Elser <josh.elser@gmail.com>
2014-06-03 18:07:37 -0400

    Remove unnecessary config, switch column configuration from | to :

--- src/test/hql/acled/query_acled.sql
+++ src/test/hql/acled/query_acled.sql
@@ -1,22 +1,14 @@
-add jar /usr/local/lib/accumulo/lib/guava.jar;
-add jar /usr/local/lib/zookeeper/zookeeper-3.4.5.jar;
-add jar /usr/local/lib/accumulo/lib/accumulo-trace.jar;
-add jar /usr/local/lib/accumulo/lib/accumulo-fate.jar;
-add jar /usr/local/lib/accumulo/lib/accumulo-core.jar;
-add jar /usr/local/lib/accumulo/lib/accumulo-start.jar;
-add jar /Users/jelser/projects/hive.git/packaging/target/apache-hive-0.14.0-SNAPSHOT-bin/apache-hive-0.14.0-SNAPSHOT-bin/lib/hive-accumulo-handler-0.14.0-SNAPSHOT.jar;
-
 DROP TABLE IF EXISTS acled;
-CREATE EXTERNAL TABLE acled(rowid STRING, lat DOUBLE, lon DOUBLE, loc STRING, src STRING, type STRING, fid INT, pid BIGINT) 
+CREATE EXTERNAL TABLE acled(rowid STRING, lat DOUBLE, lon DOUBLE, loc STRING, src STRING, type STRING) 
 STORED BY 'org.apache.hadoop.hive.accumulo.AccumuloStorageHandler' 
-WITH SERDEPROPERTIES ('accumulo.columns.mapping' = 'rowID,cf|lat,cf|lon,cf|loc,cf|src,cf|type,cf|fid,cf|pid',
+WITH SERDEPROPERTIES ('accumulo.columns.mapping' = 'rowID,cf:lat,cf:lon,cf:loc,cf:src,cf:type',
 	'accumulo.table.name' = 'acled');
 	
-select pid from acled where pid != 1111 limit 100;
+--select pid from acled where pid != 1111 limit 100;
 		
-select type,lat,lon from acled where pid = 3333 and fid = 20 and type like 'Violence%';
+--select type,lat,lon from acled where pid = 3333 and fid = 20 and type like 'Violence%';
 
-select pid,fid from acled where pid > 3333 and fid < 30 limit 100;
+--select pid,fid from acled where pid > 3333 and fid < 30 limit 100;
 
 select lat,lon from acled where lat > 0.0 and lat < 10.0 and lon > 0.0 and lon < 10.0 limit 100;
 
@@ -27,7 +19,7 @@ select * from acled where loc = 'Clough Creek';
 DROP TABLE IF EXISTS acled2;
 CREATE EXTERNAL TABLE acled2(rowid STRING, lat DOUBLE, lon DOUBLE, loc STRING, src STRING, type STRING) 
 STORED BY 'org.apache.hadoop.hive.accumulo.AccumuloStorageHandler' 
-WITH SERDEPROPERTIES ('accumulo.columns.mapping' = 'rowID,cf|lat,cf|lon,cf|loc,cf|src,cf|type',
+WITH SERDEPROPERTIES ('accumulo.columns.mapping' = 'rowID,cf:lat,cf:lon,cf:loc,cf:src,cf:type',
 	'accumulo.no.iterators' = 'true',
 	'accumulo.table.name' = 'acled');
 
